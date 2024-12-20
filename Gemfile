69fde09fabcdfc52f5f4e3ec9c9a1a92ef7f1d7e
Daniel Silvestre <djlebersilvestre@gmail.com>
2024-12-19 16:52:11 -0800

    Minor adjustments after testing locally with other apps

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 group :development, :test do
+  # TODO: add to dev dependencies in gemfile spec
   gem 'rake'
   gem 'minitest'
   gem 'mocha'
--- lib/files/json.rb
+++ lib/files/json.rb
@@ -25,6 +25,10 @@ module Files
     def parse_json(json, not_found_result)
       return not_found_result if json&.strip&.empty? && not_found_result != :raise
       JSON.parse(json)
+      # TODO: how to handle the send_email_queue bug?
+    rescue JSON::ParserError => e
+      raise e if not_found_result == :raise
+      not_found_result
     end
   end
 end
